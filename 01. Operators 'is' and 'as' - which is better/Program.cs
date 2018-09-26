1311f579baf78a8ec106762e0e27c831fbbb4b1d
aenen <yaroslav.golyuk@yandex.ua>
2018-09-26 19:25:44 +0300

    Add project files.

---
+++ 01. Operators 'is' and 'as' - which is better/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Operators__is__and__as____which_is_better
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
